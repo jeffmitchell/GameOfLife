4639bc400bc164a67a73ce195402b288ef0fb390
sarahHarvey369 <sharvey2@radford.edu>
2018-11-17 10:48:13 -0500

    testing CR

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -24,24 +24,44 @@ import static org.junit.Assert.assertTrue;
 
 public class GameOfLife {
 
-  private Boolean[][] grid = new Boolean[8][8];
+  private Boolean[][] grid = new Boolean[3][3];
 
   public static void main(String[] args) {
     GameOfLife gameOfLife = new GameOfLife();
   }
 
-  @Before
-  public void setup() {
+//  @Before
+//  public void setup() {
+//    for (int row=0; row < grid.length; row++) {
+//      for (int column = 0; column < grid.length; column++) {
+//        grid[row][column] = false;
+//      }
+//    }
+//  }
+
+  public void generate() {
     for (int row=0; row < grid.length; row++) {
       for (int column = 0; column < grid.length; column++) {
         grid[row][column] = false;
+
+        if (grid[row][column] = true) {
+          if (grid[row][column + 1] = true && grid[row + 1][column] = true)
+        }
+
       }
     }
   }
 
   @Test
-  public void testSomething() {
+  public void testLiveCell() {
+    grid[0][0] = true;
+    generate();
     assertThat(grid[0][0], is(equalTo(false)));
+  }
+
+  @Test
+  public void testGridInitialized() {
+    assertThat(grid.length > 0, is(equalTo(true)));
   }
 
 }
